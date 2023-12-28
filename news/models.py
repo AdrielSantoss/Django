84dfc9e1b64f0c96146f595a05d1a6761eec5d4b
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-12-28 20:32:02 -0300

    News: Adiciona a primeira model.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class New(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=65) #VARCHAR
+    description = models.TextField()
+    time_to_read = models.IntegerField()
+    deprecated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add: Ao ser criado, a data será registrada
+    update_at = models.DateTimeField(auto_now=True) # auto_now: Ao ser atualizado, a data será registrada
+    cover = models.ImageField(upload_to='news/covers/%Y/%m/%d/')
