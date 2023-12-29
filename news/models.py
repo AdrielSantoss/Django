5ac0db4bf0d112354fea956d9d70c389a7416b10
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-12-28 21:13:08 -0300

    News: Adiciona novas models e migrations.

--- news/models.py
+++ news/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 class New(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -11,3 +15,10 @@ class New(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # auto_now_add: Ao ser criado, a data será registrada
     update_at = models.DateTimeField(auto_now=True) # auto_now: Ao ser atualizado, a data será registrada
     cover = models.ImageField(upload_to='news/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )  # on_delete=models.SET_NULL: quando a categoria de referencia for apagada, a new terá o campo "category" como NULL |  # null=True: o campo pode ser null
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+     
