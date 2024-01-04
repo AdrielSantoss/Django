63792ef5a8a8b29f13a966f862050bda54ba3e2c
Adriel dos Santos <adrieldossantos7@gmail.com>
2024-01-04 20:30:26 -0300

    News: Corrige bugs de views e propriedades invalidas.

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,6 @@ from news import views
 
 urlpatterns = [
     path('', views.Home),
-    path('new/<int:id>/', views.New),
+    path('new/<int:id>/', views.NewPage),
     path('admin', admin.site.urls),
 ]
